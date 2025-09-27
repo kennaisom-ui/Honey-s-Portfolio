be0244fcc67d2da62940b4942c9291b5ad369c47
kennaisom-ui <kennedya.isom@gmail.com>
2025-09-27 19:56:05 -0400

    Add files via upload

---
+++ WebDevLab01/WebDevLab01/info.py
@@ -0,0 +1,97 @@
+
+#This File will contain the information to be displayed in your portfolio
+
+profile_picture = "/mount/src/honey-s-portfolio/WebDevLab01/images/profile.jpg"
+about_me = "My name is Honey. I am a Cockapoo and my job is being adorable and mischevious."
+
+# Image URLs
+pawmates_image_url = "images/pawmates.jpg"
+github_image_url = "https://cdn-icons-png.flaticon.com/256/25/25231.png"
+email_image_url = "https://logowik.com/content/uploads/images/513_email.jpg"
+
+# Personal Links
+my_pawmates_url = "https://www.pawmatesapp.com/honey-valentine-44b857281/"
+my_github_url = "https://github.com/honey-valentine"
+my_email_address = "mailto:TheDogDaysAreNotOver@gmail.com"
+
+# Education Data
+education_data = {
+    'Degree': 'Doggy Diploma',
+    'Institution': 'Hometrained',
+    'Location': 'Atlanta, GA',
+    'Graduation Date': 'Never',
+    'GPA': '0.0'
+}
+
+course_data = {
+    "code": ["PT 1101", "Tr 1203", "Ba 1102", "Pl 2000"], 
+    "names": ["Intro to Potty Training", "Tricks: Sit, Stand, Jump!", "Barking for Beginners", "Playing Nice"], 
+    "semester_taken": ["2nd", "3rd", "2nd", "2nd"],
+    "skills": ["Puppy dog eyes", "Doing tricks (but only for treats)", "Being cute", "Making people happy"]
+}
+
+# Professional Experience 
+experience_data = {
+    "Chasing": (
+        ["- I try to chase random people everyday, but my human does not seem to appreciate that",
+         "- Made owner tighten leash and call my names multiple times"],
+        "images/running.jpg"
+    ),
+    "Professional Kicker": (
+        ["- I kick dirt each time after I potty even though I completely miss",
+         "- For every 10 times I poop, there is on average 1 time I almost actually get it covered"],
+        "images/kicking.jpg"
+    ),
+    "Ultimate Cuddler": (
+        ["- I greet my human at the door everyday when they come home and cuddle"],
+        "images/cuddle.jpg"
+    )
+}
+
+# Projects Data
+projects_data = {
+    "None": "I don't have opposable thumbs, so I can't do a lot of the things humans can.",
+}
+
+# Skills Data
+skills = {
+    "Python": 0,
+    "Java": 0,
+    "C": 0,
+}
+
+# Programming Icons
+programming_icons = {
+    "Python": "🐍",
+    "Java": "☕",
+    "C": "🔍",
+}
+
+# Spoken Language Icons
+spoken_icons = {
+    "Dog": "🐾",
+    "English": "🇬🇧",
+}
+
+# Languages Data
+languages = {
+    "Dog": "Fluent",
+    "English": "Intermediate",
+}
+
+# Leadership Data
+leadership_data = {
+    "Pseudo mother of neighbor's dog": (
+        ["- 'A single mom who works two jobs, who loves her kids and never stops. With a gentle hand and the heart of a fighter. I'm a survivor.'"],
+        "images/mom.jpg"
+    )
+}
+
+# Activity Data
+activity_data = {
+    "Making People Smile Club": (
+        ["- I always cheer up my owner",
+         "- I let kids pet me even though I don't like getting pet by strangers."],
+        "images/act.jpg"
+    )
+}
